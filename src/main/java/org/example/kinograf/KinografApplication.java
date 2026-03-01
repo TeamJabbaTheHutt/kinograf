4335a2cc3f3280fe539904196a656b29582748e1
nikholt1 <niklas1997@live.dk>
2026-03-01 17:06:54 +0100

    init

---
+++ src/main/java/org/example/kinograf/KinografApplication.java
@@ -0,0 +1,13 @@
+package org.example.kinograf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinografApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KinografApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/kinograf/KinografApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.kinograf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinografApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
