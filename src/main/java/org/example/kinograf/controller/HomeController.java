20a4bc1183e06fed6e2bc812c3b3c378a5e920a3
nikholt1 <140625263+nikholt1@users.noreply.github.com>
2026-03-05 16:12:10 +0100

    theatre implementation

--- src/main/java/org/example/kinograf/controller/HomeController.java
+++ src/main/java/org/example/kinograf/controller/HomeController.java
@@ -1,4 +1,64 @@
 package org.example.kinograf.controller;
 
+import org.example.kinograf.DTO.CreateTheatreRequest;
+import org.example.kinograf.DTO.TheatreDTO;
+import org.example.kinograf.service.TheatreService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
 public class HomeController {
+    private final TheatreService theatreService;
+
+    public HomeController(TheatreService theatreService) {
+        this.theatreService = theatreService;
+    }
+
+
+
+
+
+
+    /// controller -> theatre, hvis vi mangler det
+
+    @GetMapping("/theatres")
+    public List<TheatreDTO> getTheatres() {
+        return theatreService.getAllTheatres();
+    }
+
+    @GetMapping("/theatres/{id}")
+    public ResponseEntity<TheatreDTO> getTheatre(@PathVariable Long id) {
+        Optional<TheatreDTO> theatre = theatreService.getTheatreById(id);
+        if (theatre.isPresent()) {
+            return ResponseEntity.ok(theatre.get());
+        }
+        return ResponseEntity.notFound().build();
+    }
+    @PostMapping("/theatre")
+    public ResponseEntity<TheatreDTO>  createTheatre(@RequestBody CreateTheatreRequest request) {
+        TheatreDTO saved = theatreService.createTheatre(
+                request.theatreName(),
+                request.capacity()
+        );
+        return ResponseEntity.ok(saved);
+    }
+
+    @PutMapping("/theatres/{id}")
+    public ResponseEntity<TheatreDTO> updateTheatre(@PathVariable Long id, @RequestBody CreateTheatreRequest request) {
+        TheatreDTO saved = theatreService.updateTheatre(
+                id,
+                request.theatreName(),
+                request.capacity()
+        );
+        return ResponseEntity.ok(saved);
+    }
+
+    @DeleteMapping("/theatres/{id}")
+    public ResponseEntity<TheatreDTO> deleteTheatre(@PathVariable Long id) {
+        theatreService.deleteTheatre(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
