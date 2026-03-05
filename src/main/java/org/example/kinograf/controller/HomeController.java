4f0180bd47639a9a42e0201561e39fb904d21d9c
Ella Costello <ella.costello@yahoo.com>
2026-03-05 09:53:35 +0100

    Added getMapping for index

--- src/main/java/org/example/kinograf/controller/HomeController.java
+++ src/main/java/org/example/kinograf/controller/HomeController.java
@@ -1,4 +1,21 @@
 package org.example.kinograf.controller;
 
+import org.example.kinograf.repository.MovieRepository;
+import org.example.kinograf.service.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/home")
 public class HomeController {
+
+    // For fetching info on movies to show on startpage
+
+    @GetMapping()
+    public String showHome() {
+        return "index";
+    }
+
 }
