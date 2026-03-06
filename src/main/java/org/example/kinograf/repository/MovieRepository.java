18fb432fd8bee4f455c6c84426a7ac2a324b4729
Ella Costello <ella.costello@yahoo.com>
2026-03-06 14:39:40 +0100

    index.html ready to go, with connection to API, fixed annotations, added fields to Entity

--- src/main/java/org/example/kinograf/repository/MovieRepository.java
+++ src/main/java/org/example/kinograf/repository/MovieRepository.java
@@ -2,6 +2,8 @@ package org.example.kinograf.repository;
 
 import org.example.kinograf.model.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MovieRepository extends JpaRepository<Movie, Long> {
 }
--- src/main/java/org/example/kinograf/repository/SeatRepository.java
+++ src/main/java/org/example/kinograf/repository/SeatRepository.java
@@ -2,6 +2,8 @@ package org.example.kinograf.repository;
 
 import org.example.kinograf.model.Seat;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SeatRepository extends JpaRepository<Seat, Long> {
 }
