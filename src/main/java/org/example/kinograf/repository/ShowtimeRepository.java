4e7cbb72becb8ea959ecac8138ba7bcc84012380
Andreas Holgersen <Andr9753@gmail.com>
2026-03-06 11:07:28 +0100

    ShowTime doneso

--- src/main/java/org/example/kinograf/repository/ShowtimeRepository.java
+++ src/main/java/org/example/kinograf/repository/ShowtimeRepository.java
@@ -1,4 +1,8 @@
 package org.example.kinograf.repository;
 
-public class ShowtimeRepository {
+import org.example.kinograf.model.Reservation;
+import org.example.kinograf.model.ShowTimes;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ShowtimeRepository extends JpaRepository<ShowTimes, Long> {
 }
