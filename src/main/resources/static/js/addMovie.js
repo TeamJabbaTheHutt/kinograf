a31c414c40034bf974b91f0c1d313c64287c4c0e
Ella Costello <ella.costello@yahoo.com>
2026-03-10 12:35:38 +0100

    test

--- src/main/resources/static/js/addMovie.js
+++ src/main/resources/static/js/addMovie.js
@@ -1,9 +1,29 @@
-let timer;
 
-document.getElementById("movieSearch").addEventListener("input", () => {
-    clearTimeout(timer);
-    timer = setTimeout(searchMovies, 300);
-});
+
+export async function renderAddMovie(main) {
+    main.innerHTML = "";
+
+    main.innerHTML = "<label for=\"movieSearch\"></label>\n" +
+        "    <input id=\"movieSearch\" type=\"text\" placeholder=\"Search movie\">\n" +
+        "    <div id=\"searchResults\"></div>\n" +
+        "\n" +
+        "    <div id=\"movieDetails\" style=\"border:1px solid #ccc; padding:15px; max-width:500px;\">\n" +
+        "        <img id=\"moviePoster\" src=\"\" alt=\"Movie Poster\" style=\"width:100%; max-width:300px;\">\n" +
+        "        <h2 id=\"movieTitle\"></h2>\n" +
+        "        <p><strong>Description:</strong> <span id=\"movieDescription\"></span></p>\n" +
+        "        <p><strong>Director:</strong> <span id=\"movieDirector\"></span></p>\n" +
+        "        <p><strong>Age rating:</strong> <span id=\"movieAge\"></span></p>\n" +
+        "        <p><strong>IMDB score:</strong> <span id=\"movieRating\"></span></p>\n" +
+        "    </div>"
+
+    let timer;
+
+    document.getElementById("movieSearch").addEventListener("input", () => {
+        clearTimeout(timer);
+        timer = setTimeout(searchMovies, 300);
+    });
+}
+
 async function searchMovies(){
     const apiKey = "b9c41db5";
     const query = document.getElementById("movieSearch").value;
