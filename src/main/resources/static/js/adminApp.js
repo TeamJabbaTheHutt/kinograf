160a65e6270da01cf6f0cac204038c3dd4e95a9b
iotuz <theismillinger@gmail.com>
2026-03-12 10:25:19 +0100

    addShowtime to movie with movie id theatre id and time

--- src/main/resources/static/js/adminApp.js
+++ src/main/resources/static/js/adminApp.js
@@ -1,7 +1,7 @@
 import { renderAddMovie } from "./addMovie.js";
 // import { renderEditReservation} from "./editReservation";
 import {ADMIN_BASE_URL} from "./config.js";
-
+import { renderAddShowtime } from "./addShowTime.js";
 document.addEventListener("DOMContentLoaded", initApp);
 
 async function initApp() {
@@ -10,6 +10,11 @@ async function initApp() {
     const navLinkHome = document.getElementById("navlink-Home")
     const navLinkAddMovie = document.getElementById("nav-link-add-movie");
    // const navLinkEditReservation = document.getElementById()
+    const navLinkAddShowtime = document.getElementById("nav-link-add-showtime");
+    const btnAddShowtime = document.getElementById("btn-add-showtime");
+
+    btnAddShowtime.addEventListener("click", () => renderAddShowtime(main));
+    navLinkAddShowtime.addEventListener("click", () => renderAddShowtime(main));
 
     navLinkHome.addEventListener("click", () =>  window.location.reload());
     navLinkAddMovie.addEventListener("click", () => renderAddMovie(main));
